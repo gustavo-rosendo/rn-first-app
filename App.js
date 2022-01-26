c56bb990ebe47b2475218fb457be17e9ffc2b4fe
Gustavo M. R. Santos <gustavo.rosendo@b-plat.net>
2022-01-26 20:17:13 -0300

    - first modif to some components and styling

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+var toggle = true;
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Gustavo Matsumoto Rosendo dos Santos');
+
   return (
     <View style={styles.container}>
+      <Text>Hi, I'm {outputText}.</Text>
+      <Text>This is my first React Native app! :-)</Text>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        title='Change Text' 
+        onPress={() => {
+          toggle = !toggle; 
+          setOutputText(toggle? 'Gustavo Matsumoto Rosendo dos Santos':'the Software Engineer');}} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4fac',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: '10%',
   },
 });
